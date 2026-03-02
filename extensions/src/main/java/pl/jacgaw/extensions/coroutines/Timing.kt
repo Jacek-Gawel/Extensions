25175a5f05f8df16ab2d8aa0afab5843814b2ff0
JackoxD <jacekgaw@gmail.com>
2026-03-02 07:05:15 +0100

    Add emitLatestEvery function

---
+++ extensions/src/main/java/pl/jacgaw/extensions/coroutines/Timing.kt
@@ -0,0 +1,21 @@
+package pl.jacgaw.extensions.coroutines
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.transformLatest
+import kotlinx.coroutines.isActive
+import kotlin.time.Duration
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun <T> Flow<T>.emitLatestEvery(period: Long): Flow<T> =
+    transformLatest { value ->
+        while (currentCoroutineContext().isActive) {
+            emit(value)
+            delay(period)
+        }
+    }
+
+fun <T> Flow<T>.emitLatestEvery(period: Duration): Flow<T> =
+    emitLatestEvery(period.inWholeMilliseconds)
